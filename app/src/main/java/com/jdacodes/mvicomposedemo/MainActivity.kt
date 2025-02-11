4bb0ccbe68391225caf8cfc9ccdb2ae45067fcf0
jdacodes <jda.codes@gmail.com>
2025-02-11 13:43:39 +0800

    feat: Improve splash screen experience and add full screen mode
    -   Enhanced the splash screen by making it full screen.
    -   Hid the status and navigation bars in `MainActivity` for a cleaner look.
    -   Added a transparent status and navigation bar theme in `themes.xml`.
    -   Updated the `App` composable modifier in `MainActivity` to add `statusBarsPadding`.
    - Removed unnecessary run configuration file `deploymentTargetSelector`.

--- app/src/main/java/com/jdacodes/mvicomposedemo/MainActivity.kt
+++ app/src/main/java/com/jdacodes/mvicomposedemo/MainActivity.kt
@@ -1,34 +1,37 @@
 package com.jdacodes.mvicomposedemo
 
 import android.animation.ObjectAnimator
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.animation.OvershootInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.core.animation.doOnEnd
-import org.koin.androidx.viewmodel.ext.android.viewModel
-
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-
 import androidx.navigation.compose.rememberNavController
 import com.jdacodes.mvicomposedemo.auth.presentation.splash.presentation.SplashViewModel
 import com.jdacodes.mvicomposedemo.core.presentation.App
 import com.jdacodes.mvicomposedemo.navigation.util.AuthGraph
 import com.jdacodes.mvicomposedemo.navigation.util.HomeGraph
 import com.jdacodes.mvicomposedemo.ui.theme.MviComposeDemoTheme
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : ComponentActivity() {
     private val viewModel: SplashViewModel by viewModel()
+    @SuppressLint("NewApi")
     override fun onCreate(savedInstanceState: Bundle?) {
         val splashScreen = installSplashScreen().apply {
             setOnExitAnimationListener { viewProvider ->
@@ -59,6 +62,11 @@ class MainActivity : ComponentActivity() {
             viewModel.isLoading.value
         }
         enableEdgeToEdge()
+        val windowCompat = WindowCompat.getInsetsController(window, window.decorView)
+        windowCompat.hide(WindowInsetsCompat.Type.navigationBars())
+        windowCompat.hide(WindowInsetsCompat.Type.statusBars())
+        windowCompat.systemBarsBehavior =WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+
         setContent {
             MviComposeDemoTheme {
                 val navController = rememberNavController()
@@ -85,7 +93,7 @@ class MainActivity : ComponentActivity() {
                     App(
                         modifier = Modifier
                             .background(MaterialTheme.colorScheme.background)
-                            .fillMaxSize(),
+                            .fillMaxSize().statusBarsPadding(),
                         navController = navController,
                         startDestination = startDestination
                     )
